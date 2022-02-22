aabe3fcea810c84baf238f5db5320ddad741ec78
TVSK <tvsk101@gmail.com>
2022-02-22 08:36:12 +0300

    base

---
+++ ObserverPattern/ClassLibrary/Entities/ConcreteObserver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.Interfaces;
+
+namespace ClassLibrary.Entities
+{
+    public class ConcreteObserver : IObserver
+    {
+        private int counter = 0;
+        public void upDate()
+        {
+            counter = counter + 1;
+        }
+
+        public int GetCounter()
+        {
+            return counter;
+        }
+    }
+}
---
+++ ObserverPattern/ClassLibrary/Entities/ConcreteSubject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.Interfaces;
+
+namespace ClassLibrary.Entities
+{
+    public class ConcreteSubject : ISubject
+    {
+        private string name;
+        private List<IObserver> observers = new List<IObserver>();
+
+        public void NotifyOdserver()
+        {
+            foreach (IObserver observer in observers) observer.upDate();
+        }
+
+        public void RegisterObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public string GetState()
+        {
+            return name;
+        }
+
+        public void SetState(string Name)
+        {
+            name = Name;
+            NotifyOdserver();
+        }
+    }
+}
